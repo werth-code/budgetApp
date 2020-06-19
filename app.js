5efaea9a5bde8feb533b7f2ae7566f03894fa292
Matt Werth <matthewwerth@gmail.com>
2020-06-19 08:41:49 -0400

    Added Module Pattern Functions to app.js

--- app.js
+++ app.js
@@ -4,4 +4,32 @@ const budgetController = (function() {
 
     const add = a => x + a
 
+    return {
+        publicTest: function(b) {
+            return add(b)
+        }
+    }
+
 })()
+
+////////////////////
+
+const UIController = (function() {
+
+    // code
+
+})() 
+
+////////////////////
+
+const controller = (function(budgetCtrl, UICtrl) {
+
+    const test = budgetCtrl.publicTest(5)
+
+    return {
+        anotherPublic: function() {
+            console.log(test)
+        }
+    }
+
+})(budgetController, UIController)
